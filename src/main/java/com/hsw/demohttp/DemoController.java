03b2c370592c6e43592db4f07b14e2ead48b8b65
胡舒威 <569017737@qq.com>
2020-07-21 19:38:35 +0800

    “第一次提交

---
+++ src/main/java/com/hsw/demohttp/DemoController.java
@@ -0,0 +1,17 @@
+package com.hsw.demohttp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping(value = "/people")
+    public People getPeople(){
+        People people = new People();
+        people.setName("王二");
+        people.setAge(20);
+        people.setSex("男");
+        return people;
+    }
+}
---
+++ src/test/java/com/hsw/demohttp/DemoHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsw.demohttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
